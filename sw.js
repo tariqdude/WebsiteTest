b0641991200f14198ffbbbe3ae9f9836ad287f1f
Tariq <3333752+tariqdude@users.noreply.github.com>
2025-06-04 16:57:45 -0500

    1

--- sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
-workbox.precaching.precacheAndRoute([
-  {url: '/', revision: null},
-  {url: '/index.html', revision: null},
-  {url: '/styles.css', revision: null},
-  {url: '/main.js', revision: null}
-]);
-workbox.routing.registerRoute(({request}) => request.mode === 'navigate', new workbox.strategies.NetworkFirst({cacheName: 'pages'}));
