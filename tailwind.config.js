126fe91590a0cfb2520f6a116b34ff71889a2a79
Tariq <3333752+tariqdude@users.noreply.github.com>
2025-06-04 15:51:41 -0500

    Add interactive test demo page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     "./apexbuild/layouts/**/*.html",
     "./**/*.html"
   ],
+  safelist: [
+    'cube-container',
+    'cube',
+    'face',
+    'grid-demo',
+    'webgl-canvas',
+    'particle-canvas',
+    'paint-box',
+    'observer-target',
+    'fetch-output',
+    'osc-controls',
+    'speak-btn'
+  ],
   theme: {
     extend: {},
   },
