788cb37512ebf9bb887b603ab38a57287fe1349a
Tariq <3333752+tariqdude@users.noreply.github.com>
2025-06-04 18:34:57 -0500

    Add offline caching service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,30 @@
-self.addEventListener('install', e => {
+const CACHE = 'site-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        '/index.html',
+        '/offline.html',
+        '/styles.css',
+        '/scripts.js',
+        'https://placehold.co/96x96',
+        'https://placehold.co/180x180',
+        'https://placehold.co/1920x1080',
+        'https://placehold.co/800x600',
+        'https://placehold.co/400',
+        'https://placehold.co/1200x630'
+      ]);
+    })
+  );
   self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  // simple passthrough
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match('offline.html'));
+    })
+  );
 });
 
