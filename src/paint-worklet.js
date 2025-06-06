eebabbaf5a3a44f0bf5a7256b63f48ea02f72297
Tariq <3333752+tariqdude@users.noreply.github.com>
2025-06-06 13:31:43 -0500

    1

--- src/paint-worklet.js
+++
@@ -1,8 +0,0 @@
-// paint-worklet.js
-/**
- * @module paintWorklet
- * Purpose: Register CSS Paint Worklet for procedural steel I-beam drawing.
- */
-if ('CSS' in window && CSS.paintWorklet) {
-  CSS.paintWorklet.addModule('paint-worklet1.js');
-}
