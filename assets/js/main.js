acad7685b441258215bc40d181de68ab4f850b7f
Tariq <3333752+tariqdude@users.noreply.github.com>
2025-06-07 22:14:37 -0500

    1

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// Initialize AOS animations
-AOS.init();
-
-// Service Worker registration for Workbox
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
-}
