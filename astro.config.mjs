9e0a1c71f77e4549ebe1d2deeed538a424bd7f78
Tariq <3333752+tariqdude@users.noreply.github.com>
2025-07-13 23:38:00 -0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tariqdude.github.io',
   base: '/websitetest',
   output: 'static',
   integrations: [tailwind()],
