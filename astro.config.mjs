9c57ec5c75d17748db713c5e4b64baafef9f81f3
Tariq <3333752+tariqdude@users.noreply.github.com>
2025-07-20 20:47:10 -0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static'
+  server: {
+    port: 3000,
+    host: true
+  }
 });
