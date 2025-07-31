00c04b7191e25664808a68e1bb0e7b864eb3f03f
tariqdude <tariqdude@users.noreply.github.com>
2025-07-31 00:55:30 -0500

    1

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tariqdude.github.io',
   base: '/WebsiteTest', // Replace with your repository name
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
